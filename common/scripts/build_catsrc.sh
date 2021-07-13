fd8371d842f2549eda5634abeb579c559fcb6ffe
Pawel Kopel <Pawel.Kopel@ibm.com>
2021-07-13 11:44:50 +0200

    Add 'docker login' for user-defined registry. Fix errors on deleting temp directory.

--- common/scripts/build_catsrc.sh
+++ common/scripts/build_catsrc.sh
@@ -82,6 +82,10 @@ function setup() {
         info "log in to container registry"
         $CONTAINER_CLI login "$SCRATCH_REG" -u "$ARTIFACTORY_USER" -p "$ARTIFACTORY_TOKEN"
         $CONTAINER_CLI login "$COMMON_SERVICE_BASE_REGISTRY" -u "$ARTIFACTORY_USER" -p "$ARTIFACTORY_TOKEN"
+        if [[ "$REGISTRY" != "" && "$REGISTRY" != "$SCRATCH_REG" ]]; then
+            $CONTAINER_CLI login "$REGISTRY" -u "$ARTIFACTORY_USER" -p "$ARTIFACTORY_TOKEN"
+        fi
+
     fi
     RELEASE_VERSION=$(cat RELEASE_VERSION)
     CROSSPLANE_BRANCH=$(git branch --show-current)
@@ -316,7 +320,7 @@ function create_index() {
         update_registry "$PATH_TO_DB"
     elif [[ "$CONTAINER_CLI" == "podman" ]]; then
         update_index
-    else 
+    else
         erro "unknown container cli: $CONTAINER_CLI"
     fi
     local LOCAL_CATSRC_IMG="$SCRATCH_REG/$NEW_CUSTOM_CATSRC:$TIMESTAMP"
