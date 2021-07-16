1d5deaa51463b406c7534d1704ec156ed97941d8
Pawel Kopel <Pawel.Kopel@ibm.com>
2021-07-16 12:21:42 +0200

    Split step 'Build images' to 'Build ibm-crossplane-operator images' and 'Build ibm-crossplane images'

--- common/scripts/install_tools.sh
+++ common/scripts/install_tools.sh
@@ -28,7 +28,6 @@ if [[ ! -d "$TOOLS_DIR" ]]; then
     mkdir "$TOOLS_DIR"
 fi
 
-
 function check_yq() {
     if [[ $(which yq) != "" ]]; then
         YQ=$(which yq)
@@ -80,7 +79,6 @@ function check_opm() {
     echo -n "opm version: " && $OPM version | cut -f2 -d\"
 }
 
-
 function check_container_cli() {
     if [[ $(which podman) != "" ]]; then
         export CONTAINER_CLI="podman"
@@ -101,7 +99,7 @@ function check_curl() {
         echo "no curl executable in \$PATH"
         exit 1
     fi
-    echo -n "curl version: " && $CURL --version | grep "^curl" | cut -f2 -d' ' 
+    echo -n "curl version: " && $CURL --version | grep "^curl" | cut -f2 -d' '
 }
 
 check_container_cli
